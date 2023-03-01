fde7b9578b66d1bd49cf4a4143dd17cd8c471556
veer277 <90796821+veer277@users.noreply.github.com>
2023-03-01 12:40:56 -0600

    Added logo component

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from './components/Logo'
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <Logo
+        path={require("./assets/logo.png")}
+        width = {200}
+        height = {200}
+      />
       <StatusBar style="auto" />
     </View>
   );
