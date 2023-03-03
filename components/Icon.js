01ff94cf6667803131cd9ea11ae668689f5c9f72
veer277 <90796821+veer277@users.noreply.github.com>
2023-03-03 12:48:30 -0600

    Created Icon Component

---
+++ components/Icon.js
@@ -0,0 +1,20 @@
+import React form 'react'
+import { StyleSheet } from 'react-native';
+import {Entypo} from '@expo/vector-icons'
+
+function Icon({name, size, color}) {
+    return (
+        <Entypo
+            name={name}
+            size={size}
+            style={[styles.icon, {color: color}]}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 40, 
+        height: 40
+    }
+})
