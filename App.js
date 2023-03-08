9dcf06be241f178b7c354b6d8d5a825c99b3689c
schenna217 <90796839+schenna217@users.noreply.github.com>
2023-03-08 12:15:29 -0600

    Imported screens to make buttons

--- App.js
+++ App.js
@@ -4,6 +4,18 @@ import Logo from './components/Logo'
 import React, { useState } from 'react';
 import { StackActions } from '@react-navigation/native';
 
+import Cart from './screens/Cart';
+import Confirmation from './screens/Confirmation';
+import Drinks from './screens/Drinks';
+import Favorites from './screens/Favorites';
+import Home from './screens/Home';
+import Item from './screens/Item';
+import Meals from './screens/Meals';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import Snacks from './screens/Snacks';
+import Surprise from './screens/SurpriseMe';
+
 
 export default function App() {
 
@@ -33,6 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'left',
   },
 });
