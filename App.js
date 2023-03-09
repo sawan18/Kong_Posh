225faa6ee49c2e13de4b5a12276149acd6d0f219
MiheerDGH <90772705+MiheerDGH@users.noreply.github.com>
2023-03-09 12:51:59 -0600

    started adding screens

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 import Logo from './components/Logo'
 import Button from './components/Button'
-import { StackActions } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {SignIn} from './screens/SignIn'
+import {SignUp} from './screens/SignUp'
 
 
-export default function App({navigation}) {
+export default function App() {
 
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
@@ -22,8 +24,26 @@ export default function App({navigation}) {
       <StatusBar style="auto" /> 
      <Button 
         title='Go to Third Screen'
-        onPress={() => navigation.navigate("SignUp")}
+        onPress={() => this.props.navigation.navigate("SignUp")}
       />
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="SignIn"
+          component={SignIn}
+          options={{title: "Welcome"}}
+        />
+        <Stack.Screen 
+          name="SignUp"
+          component={SignUp}
+        />
+        <Stack.Screen 
+          name="Third"
+          component={ThirdScreen}
+          //options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     </View>
      
     
